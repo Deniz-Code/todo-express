43433327e9522afc70e29ecc868d69f0ee2e4b26
ThiefGang <104874053+ThiefGang@users.noreply.github.com>
2023-01-29 17:56:50 +0200

    add mongodb connection

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+import "dotenv/config.js";
 import createError from "http-errors";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
 import logger from "morgan";
+import "./config/database.js"
 
+//import routers
 import { router as indexRouter } from "./routes/index.js";
 import { router as todosRouter } from "./routes/todos.js";
 
+
+//set up app
 const app = express();
 
 // view engine setup
